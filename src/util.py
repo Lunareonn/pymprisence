ec9df70fba93a25f0ca77db2a65f51a884fa0627
Lunareonn <basikallyluna@gmail.com>
2025-05-06 22:34:25 +0300

    Get track cover from lastfm

--- src/util.py
+++ src/util.py
@@ -1,4 +1,6 @@
 import dbus
+import requests
+import json
 import dbus.mainloop.glib
 
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
@@ -9,11 +11,23 @@ player = bus.get_object("org.mpris.MediaPlayer2.sayonara",
 properties = dbus.Interface(player, "org.freedesktop.DBus.Properties")
 
 
+def get_cover_lastfm(track: str, artist: str):
+    api_key = "apikey"
+    url = f"http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={api_key}&artist={artist}&track={track}&format=json"
+    print(url)
+    headers = {'content-type': 'application/json'}
+    response = requests.get(url, headers=headers)
+    response_data = json.loads(response.text)
+
+    cover_url = response_data["track"]["album"]["image"][3]["#text"]
+    print(cover_url)
+
+
 def get_metadata():
     metadata = properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
 
     song_title = metadata.get("xesam:title", "Unknown Title")
-    song_artist = metadata.get("xesam:artist", "Unknown Artist")
+    song_artist = metadata.get("xesam:albumArtist", "Unknown Artist")
     song_length = round(metadata.get("mpris:length") / 1000000)
 
     return song_title, song_artist, song_length
