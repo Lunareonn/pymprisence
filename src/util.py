ab02129e4ff6161aa085174ee4ea1a78c1193193
Lunareonn <basikallyluna@gmail.com>
2025-05-06 23:42:34 +0300

    Remove debug prints

--- src/util.py
+++ src/util.py
@@ -12,15 +12,14 @@ properties = dbus.Interface(player, "org.freedesktop.DBus.Properties")
 
 
 def get_cover_lastfm(track: str, artist: str):
-    api_key = "apikey"
+    api_key = "698a7b7f60d194998373d6d52f0d57ab"
     url = f"http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={api_key}&artist={artist}&track={track}&format=json"
-    print(url)
     headers = {'content-type': 'application/json'}
     response = requests.get(url, headers=headers)
     response_data = json.loads(response.text)
 
     cover_url = response_data["track"]["album"]["image"][3]["#text"]
-    print(cover_url)
+    return cover_url
 
 
 def get_metadata():
@@ -35,6 +34,5 @@ def get_metadata():
 
 def get_position():
     position = properties.Get("org.mpris.MediaPlayer2.Player", "Position")
-    print(position)
 
     return round(position / 1000000)
