a2e979877572fc9dc6f488552ae5fd48fdedf240
Lunareonn <basikallyluna@gmail.com>
2025-05-12 12:20:33 +0300

    Remove code related to rich presence from main.py

--- src/main.py
+++ src/main.py
@@ -2,36 +2,13 @@ from pypresence import Presence, ActivityType
 from gi.repository import GLib
 from signal_handler import listener
 import asyncio
-import time
-import util
+import presence
 
-client_id = "1366680558775570504"
-RPC = Presence(client_id)
-RPC.connect()
 
-song_title, song_artist, song_length = util.get_metadata()
-position = util.get_position()
+async def main():
+    event = asyncio.Event()
+    RPC = await presence.initialize_rpc(event)
+    await presence.rpc_loop(event, RPC)
 
-RPC.update(
-    activity_type=ActivityType.LISTENING,
-    details=song_title,
-    state=song_artist,
-    start=time.time() - position,
-    end=int(song_length) + time.time() - position,
-)
-
-asyncio.run(listener())
-loop = GLib.MainLoop()
-loop.run()
-
-while True:
-    time.sleep(5)
-    position = util.get_position()
-    start_time = time.time() - position
-    RPC.update(
-        activity_type=ActivityType.LISTENING,
-        details=song_title,
-        state=song_artist,
-        start=time.time() - position,
-        end=time.time() + int(song_length) - position,
-    )
+if __name__ == "__main__":
+    asyncio.run(main())
