3a0d21edc554d2635f735eb69ee1661205560df5
Lunareonn <basikallyluna@gmail.com>
2025-05-16 11:49:25 +0300

    Add config generation and temporary player listing to main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 import asyncio
 import presence
+import configuration
+import util
 
 
 async def main():
     event = asyncio.Event()
+    players = await util.get_players()
+    print("players:")
+    for player in players:
+        print(player)
     RPC = await presence.initialize_rpc(event)
     asyncio.create_task(presence.rpc_loop(event, RPC))
 
+    await configuration.generate_config()
     await asyncio.Future()
 
 if __name__ == "__main__":
