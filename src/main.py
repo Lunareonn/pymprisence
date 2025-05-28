ccd1c97cd0bf28a717c115987d1b8a42f58fce3b
Lunareonn <basikallyluna@gmail.com>
2025-05-28 17:41:49 +0300

    Add argparser for listing players

--- src/main.py
+++ src/main.py
@@ -2,9 +2,30 @@ from logs import logger
 import asyncio
 import presence
 import configuration
+import util
+import argparse
+import sys
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--players", help="List available MPRIS players", action="store_true")
+    args = parser.parse_args()
+
+    return args
 
 
 async def main():
+    args = parse_args()
+
+    if args.players:
+        players = await util.get_players()
+        print("Available players:")
+        for player in players:
+            sanitized_player = util.sanitize_player_name(player)
+            print("-", sanitized_player)
+        sys.exit(0)
+
     await configuration.generate_config()
 
     event = asyncio.Event()
