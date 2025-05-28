ebde79e82e12feab489d32bc6b408e7ed04a0e2d
Lunareonn <basikallyluna@gmail.com>
2025-05-28 23:22:18 +0300

    Add separate logger level settings for console and file loggers

--- src/logs.py
+++ src/logs.py
@@ -10,7 +10,8 @@ try:
     with open(os.path.join(home_folder, ".config", "pymprisence", "config.toml"), "rb") as cfg:
         cfg_file = tomllib.load(cfg)
 
-    logger_level = cfg_file["logger"]["logging_level"]
+    file_logger_level = cfg_file["logger"]["file_logging_level"]
+    console_logger_level = cfg_file["logger"]["console_logging_level"]
 except FileNotFoundError:
     logger_level = "INFO"
 
@@ -20,15 +21,18 @@ logger.setLevel(logging.DEBUG)
 console_formatter = logging.Formatter("%(asctime)s %(message)s", datefmt="%H:%M")
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(console_formatter)
-console_handler.setLevel(logging.INFO)
+if console_logger_level == "DEBUG":
+    console_handler.setLevel(logging.DEBUG)
+elif console_logger_level == "INFO":
+    console_handler.setLevel(logging.INFO)
 logger.addHandler(console_handler)
 
 
 file_formatter = logging.Formatter("%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s")
 file_handler = logging.FileHandler(f"{home_folder}/.pymprisence/logs/{datetime.now().replace(microsecond=0).strftime("%d-%m-%Y-%H-%M-%S")}.log", mode="w", encoding="utf-8")
 file_handler.setFormatter(file_formatter)
-if logger_level == "DEBUG":
+if file_logger_level == "DEBUG":
     file_handler.setLevel(logging.DEBUG)
-elif logger_level == "INFO":
+elif file_logger_level == "INFO":
     file_handler.setLevel(logging.INFO)
 logger.addHandler(file_handler)
